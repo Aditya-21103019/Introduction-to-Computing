6b426006b16038e0de6688a73a6cc7e5f8f8da95
Aditya-21103019 <97451138+Aditya-21103019@users.noreply.github.com>
2022-02-14 16:31:18 +0530

    Add files via upload

---
+++ Assignment3output.py
@@ -0,0 +1,110 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\ag530\OneDrive\Documents\computing folder\Assignment3.py ==
+Solution 1:
+
+Enter string here: Aditya
+ 
+{'A': 2, 'D': 1, 'I': 1, 'T': 1, 'Y': 1}
+********************************************************************************
+Solution 2:
+
+Enter day here[1-31]:13
+Enter number of month here[1-12]:12
+Enter year here[1800-2025]:2002
+
+The date of next day is: 14/12/2002
+********************************************************************************
+Solution 3:
+
+If you don't want to input more element, type 'end' 
+ 
+Enter number 1 here:7
+Enter number 2 here:56
+Enter number 3 here:39
+Enter number 4 here:82
+Enter number 5 here:77
+Enter number 6 here:end
+
+Entered list is : [7, 39, 56, 77, 82]
+
+Output is: [(7, 49), (39, 1521), (56, 3136), (77, 5929), (82, 6724)]
+********************************************************************************
+Solution 4:
+
+Kindly enter grade point as an integer from 4-10 
+
+Enter your grade point here:9
+Your Grade is 'A' and Excellent
+********************************************************************************
+Solution 5:
+
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+********************************************************************************
+Solution 6:
+
+Type 'Y' if you want to enter details
+Type 'N' if you don't want to enter more details
+
+You want to add details? y
+Kindly add a unique name for every SID
+Enter SID here: 21103019
+Enter Name here: Aditya
+  
+You want to add details? Y
+Enter SID here: 21103018
+Enter Name here: Keshav
+  
+You want to add details? Y
+Enter SID here: 21103032
+Enter Name here: Vaibhav
+  
+You want to add details? Y
+Enter SID here: 21103047
+Enter Name here: Arin
+  
+You want to add details? N
+ 
+a)  The dictionary containing student's names and their SIDs is: 
+{21103019: 'Aditya', 21103018: 'Keshav', 21103032: 'Vaibhav', 21103047: 'Arin'}
+
+b)  The dictionary sorted by names of students is: 
+{21103019: 'Aditya', 21103047: 'Arin', 21103018: 'Keshav', 21103032: 'Vaibhav'}
+
+c)  The dictionary sorted by SIDs of students is: 
+{21103018: 'Keshav', 21103019: 'Aditya', 21103032: 'Vaibhav', 21103047: 'Arin'}
+
+d)  Enter the SID of student you want to find: 21103019
+    The name of student with SID 21103019 is: Aditya
+********************************************************************************
+Solution 7:
+
+Enter number of terms in Fibonacci sequence: 12
+ 
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+The Average of Sequence is 19.333333333333332
+********************************************************************************
+Solution 8:
+
+a.)  {1, 3, 5, 6, 8}
+b.)  {17, 3, 6, 8, 9, 13}
+c.)  {1, 2, 4, 5}
+d.)  {6, 7, 8, 9, 10}
+e.)  {10, 7}
